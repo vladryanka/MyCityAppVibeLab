009bd9a7009ecfe73ff9bc1061bdb75472eec403
vladryanka <vsmorzhok@mail.ru>
2024-03-16 22:17:03 +0300

    ура, работает и без класса навигации

--- app/src/main/java/com/example/mycityapp/data/RoutOnCity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mycityapp.data
-
-enum class RoutOnCity(val route: String) {
-        Categories("categorys"),
-        Recommendations("recommendation/{category}"),
-        RecommendationDetail("recdetail/{recId}")
-
-}
